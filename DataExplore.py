4cb47991d3937436483b2547a4ee94773d163e6b
neilgeorge03 <neilgeorge03@gmail.com>
2023-01-21 14:20:00 -0500

    Change DataSet

--- DataExplore.py
+++ DataExplore.py
@@ -1,25 +1,14 @@
 import pandas as pd
 
 
-data = pd.read_json("Dataset/AequitasData.json")
+data = pd.read_json("/Users/neiljoegeorge/Documents/ConU/Layoffsfyi_Tracker_layoffsfyitracker.2023-1-21.json")    
 
-# for columnName in data.columns:
-#     # print(f"{columnName}:{data[columnName].unique()}")
-#     print(columnName)
-    
-# print(data.columns)
-print(data["Direction"].unique())
-print(data["MessageType"].unique())
-print(data["Symbol"].unique())
-# print(data["OrderPrice"].unique())
-print(data["Exchange"].unique())
+print(data)
 
-# print(data["Direction"].unique())
-
-
-
-d = {}
 # ID -> Key
 # items -> Status, Price, Symbol
 # Approved -> move to another thing
 # Rejected/Cancelled -> remove completely we don't care
+
+
+
