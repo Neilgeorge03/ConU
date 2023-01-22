00994293c3076ff1a9fe37b92f08676b4c9ef9c9
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-01-21 19:23:51 -0500

    basic code for packed circle chart

--- DataExplore.py
+++ DataExplore.py
@@ -1,14 +1,53 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import circlify
 
+df = pd.read_json("Layoffsfyi_Tracker_layoffsfyitracker.2023-1-21.json").loc[:, ["Company", "# Laid Off"]]  
+df = df.groupby("Company", as_index=False).sum()
+df.sort_values(by="# Laid Off", ascending=False, inplace=True)
 
-data = pd.read_json("/Users/neiljoegeorge/Documents/ConU/Layoffsfyi_Tracker_layoffsfyitracker.2023-1-21.json")    
+top_50 = df.head(50)
+layoffs = top_50["# Laid Off"].tolist()
 
-print(data)
+circles = circlify.circlify(
+    layoffs,
+    show_enclosure=True,
+    target_enclosure=circlify.Circle(x=0, y=0, r=1)
+)
 
-# ID -> Key
-# items -> Status, Price, Symbol
-# Approved -> move to another thing
-# Rejected/Cancelled -> remove completely we don't care
+fig, ax = plt.subplots(figsize=(20,20))
+
+ax.axis('off')
+
+lim = max(
+    max(
+        abs(circle.x) + circle.r,
+        abs(circle.y) + circle.r,
+    )
+    for circle in circles
+)
+
+plt.xlim(-lim, lim)
+plt.ylim(-lim, lim)
+
+labels = top_50["Company"]
+
+for (circle, label) in zip(circles, labels):
+    x, y, r = circle
+    ax.add_patch(plt.Circle((x,y), r, alpha=0.2, linewidth=2, fill=False))
+    plt.annotate(
+          label, 
+          (x,y ) ,
+          va='center',
+          ha='center'
+        )
+
+plt.show()
+
+ #ID -> Key
+ #items -> Status, Price, Symbol
+ #Approved -> move to another thing
+ #Rejected/Cancelled -> remove completely we don't care
 
 
 
